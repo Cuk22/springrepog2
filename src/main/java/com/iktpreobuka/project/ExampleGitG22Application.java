b46a93429eabfccb9e2b6d1445f0013d3565382d
Cuk22 <marko.cuk.brain22@gmail.com>
2023-02-21 02:24:38 +0100

    moj prvi commit

---
+++ src/main/java/com/iktpreobuka/project/ExampleGitG22Application.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitG22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitG22Application.class, args);
+	}
+
+}
